07db630807fb37448c891bb4cc95a9a3190e9380
川原 英明 <hideaki-kawahara@bright-system.jp>
2019-01-29 01:59:50 +0900

    パターン追加

--- sample2/Jiro.class.php
+++ sample2/Jiro.class.php
@@ -1,27 +1,49 @@
 <?php
-function categoryGenerator()
-{
-    yield "性別";
-    yield from genderGenerator();
-    yield "職業";
-    yield from jobGenerator();
-    yield "wwww";
-    yield from ["aa", "bb"];
-}
+class Jiro {
+  public static $recipe = [
+    'ニンニク' => ['無し', '少なめ', '多め'],
+    '野菜' => ['無し', '普通', '増し', '増し増し']
+  ];
 
-function genderGenerator()
-{
-    yield "男";
-    yield "女";
-}
+  public static function ginger($order) {
+    foreach (Jiro::$recipe as $key => $value) {
+      if ($key === 'ニンニク') {
+        foreach ($value as $many) {
+          if ($many === $order) {
+            return TRUE;
+          }
+        }
+      }
+    }
+    return FALSE;
+  }
 
-function jobGenerator()
-{
-    yield "会社員";
-    yield "自営業";
-    yield "無職";
-}
+  public static function categoryGenerator() {
+      yield "性別";
+      yield from Jiro::genderGenerator();
+      yield "職業";
+      yield from Jiro::jobGenerator();
+      yield "wwww";
+      yield from ["aa", "bb"];
+  }
 
-foreach(categoryGenerator() as $value) {
-    print($value."\n");
+  public static function genderGenerator()
+  {
+      yield "男";
+      yield "女";
+  }
+
+  public static function jobGenerator()
+  {
+      yield "会社員";
+      yield "自営業";
+      yield "無職";
+  }
+
 }
+//print var_dump(Jiro::$recipe);
+print (Jiro::ginger('多め'));
+
+// foreach(Jiro::categoryGenerator() as $value) {
+//     print($value."\n");
+// }
